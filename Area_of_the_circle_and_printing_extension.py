4d4355c8dae4fb2b4fdbd4cb89244d7872255b6b
Saipreethi2003 <100211700+Saipreethi2003@users.noreply.github.com>
2022-03-25 18:03:37 +0530

    Create Area_of_the_circle_and_printing_extension.py

---
+++ Area_of_the_circle_and_printing_extension.py
@@ -0,0 +1,10 @@
+#Area of the circle
+import math
+radius=float(input("Enter the radius of the circle : ")
+area=math.pi*radius*radius
+print("Area of the circle is : {0}".format(area)")
+#printing extension of the given file
+filename=input("Enter the name of the file : ")
+f_exts=filename.split(".")
+print("The extension of the given filename is : ",repr(f_exts[-1]))
+
